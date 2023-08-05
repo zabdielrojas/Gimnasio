71bcf1203699ad25aa2e1b2f90f35ffea3b89db3
Susanistikis <susanam909@gmail.com>
2023-08-05 15:35:44 +0200

    mejoras de funciones

---
+++ src/controllers/exercises/deleteExercises.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+// Importamos la función que nos permite obtener una conexión libre con la base de datos.
+const getDb = require("../../db/getDb");
+// Importamos el modelo necesario.
+const deleteExerciseModel = require("../../models/exercises/deleteExerciseModel");
+
+// Función controladora  que elimina un ejercicio desde el id del ejercicio.
+const deleteExercise = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+
+    // Comprobar si el usuario tiene un rol de administrador
+    if (req.user.role !== "admin") {
+      return res.status(403).send({
+        status: "error",
+        message: "No tienes permiso para realizar esta acción",
+      });
+    }
+
+    // Eliminar el ejercicio por su ID
+    await deleteExerciseModel(id);
+
+    res.status(200).send({
+      status: "ok",
+      message: "Ejercicio eliminado",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = deleteExercise;
+getDb();
--- src/models/exercises/deleteExerciseModel.js
+++ src/models/exercises/deleteExerciseModel.js
@@ -3,14 +3,16 @@ require("dotenv").config();
 // Importamos la función que nos permite obtener una conexión libre con la base de datos.
 const getDb = require("../../db/getDb");
 
+const deletePhotoService = require("../../services/deletePhotoService");
+
 async function deleteExercisesModel(exerciseId) {
   let connection;
   try {
     connection = await getDb();
 
     // Primero verificamos que el ejercicio existe
-    const exercise = await connection.query(
-      `SELECT id FROM exercises WHERE id = ?`,
+    const [exercise] = await connection.query(
+      `SELECT id, photoName FROM exercises WHERE id = ?`,
       [exerciseId]
     );
 
@@ -18,6 +20,9 @@ async function deleteExercisesModel(exerciseId) {
     if (exercise.length === 0) {
       throw new Error("El ejercicio que intentas eliminar no existe.");
     }
+
+    // Eliminamos la foto asociada al ejercicio
+    await deletePhotoService(exercise[0].photoName);
 
     // Si todo está en orden, procedemos a eliminar el ejercicio
     await connection.query(`DELETE FROM exercises WHERE id = ?`, [exerciseId]);
